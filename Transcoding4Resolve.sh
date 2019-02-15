e48a0c24c51480afbbd3b5f19fa22924b0e5020a
p-omeilleur <p-o.meilleur@hotmail.ca>
2019-02-15 09:23:58 -0500

    move raw file to an subfolder

--- Transcoding4Resolve.sh
+++ Transcoding4Resolve.sh
@@ -6,10 +6,13 @@ read -p "1: What is video extension you want do transcode : " extension
 echo ""
 read -p "2: What is the resolution to transcode : " resolution
 
-mkdir -p output
+mkdir -p transcode
+mkdir -p raw
+
 
 for i in *.$extension;
   do name=`echo $i | cut -d'.' -f1`;
   echo $name;
-  ffmpeg -i "$i" -vcodec dnxhd -acodec pcm_s16le -s $resolution -b:v 36M -pix_fmt yuv422p -f mov output/${name}.mov
+  ffmpeg -i "$i" -vcodec dnxhd -acodec pcm_s16le -s $resolution -b:v 36M -pix_fmt yuv422p -f mov transcode/${name}.mov
+  mv -i $i raw
 done
