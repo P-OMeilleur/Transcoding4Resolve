332e5736be28d499f4040f98dd4c1f8a7ed37eb3
p-omeilleur <p-o.meilleur@hotmail.ca>
2019-02-15 09:51:03 -0500

    use raw file resolution for transcode file

--- Transcoding4Resolve.sh
+++ Transcoding4Resolve.sh
@@ -3,8 +3,6 @@ clear
 echo "--------------------Transcoding4Resolve--------------------"
 echo ""
 read -p "1: What is video extension you want do transcode : " extension
-echo ""
-read -p "2: What is the resolution to transcode : " resolution
 
 mkdir -p transcode
 mkdir -p raw
@@ -13,6 +11,6 @@ mkdir -p raw
 for i in *.$extension;
   do name=`echo $i | cut -d'.' -f1`;
   echo $name;
-  ffmpeg -i "$i" -vcodec dnxhd -acodec pcm_s16le -s $resolution -b:v 36M -pix_fmt yuv422p -f mov transcode/${name}.mov
+  ffmpeg -i "$i" -vcodec dnxhd -acodec pcm_s16le -b:v 36M -pix_fmt yuv422p -f mov transcode/${name}.mov
   mv -i $i raw
 done
